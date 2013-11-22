d63eedc2e94ccb4ec7687973752f0b96ebf16bac
Arash Ghoreyshi <arashghoreyshi@gmail.com>
2013-11-22 14:19:15 -0600

    Move generated static files to the staticfiles directory

--- stockade/settings.py
+++ stockade/settings.py
@@ -76,7 +76,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -87,11 +87,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    ('js', os.path.join(PROJECT_DIR, 'static', 'js')),
-    ('css', os.path.join(PROJECT_DIR, 'static', 'css')),
-    ('fonts', os.path.join(PROJECT_DIR, 'static', 'fonts')),
-    ('lib', os.path.join(PROJECT_DIR, 'static', 'lib')),
-    ('partials', os.path.join(PROJECT_DIR, 'static', 'partials')),
+    os.path.join(PROJECT_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
